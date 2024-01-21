e94636e95cbf078e081ea06c68b4d00615e54223
Svetlaya87 <svetlaya87@gmail.com>
2024-01-22 00:25:37 +0200

    refactor: dell comments

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -12,18 +12,6 @@ const filesCopy = path.join(__dirname, `files-copy`);
             await fs.promises.unlink(path.join(filesCopy, item.name));
         }
     }
-/*
-    fs.readdir( filesCopy, { withFileTypes: true }, function(err, files){
-     
-        for( let item of files){
-            console.log(path.join(filesCopy, item.name));
-            if ( item.isFile() ){
-
-                fs.unlink( path.join(filesCopy, item.name), (err)=> {console.log()} );
-            }
-        }
-    
-    });*/
 
     const filesToCopy = await fs.promises.readdir(filePath, { withFileTypes: true });
     
@@ -32,20 +20,6 @@ const filesCopy = path.join(__dirname, `files-copy`);
             await fs.promises.copyFile(path.join(filePath, item.name), path.join(filesCopy, item.name));
         }
     }
-
-    /*
-    let item =fs.readdir( filePath, { withFileTypes: true }, function(err, files){
-    
-        for( let item of files){
-            console.log("item ", item.name);
-            console.log("44-filesCopy ", filesCopy);
-            if ( item.isFile() ){
-
-                fs.copyFile( path.join(filePath, item.name), path.join(filesCopy, item.name), (err)=> {console.log()} );
-                
-            }
-        }
-    });*/
 
 })();
 
